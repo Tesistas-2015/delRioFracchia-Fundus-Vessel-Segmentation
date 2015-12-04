c6c531db34c3130c31772f19fec4cb46b6361ed0
fracchiamarcos <fracchiamarcos@gmail.com>
2015-12-04 17:07:50 -0300

    Se agrega segmentacion y curva roc de prueba

--- segmentation.m
+++ segmentation.m
@@ -22,4 +22,29 @@ figure('name','Imagen preprocesada sin fondo');
 imshow(ISinFondo, [min(ISinFondo(:)) max(ISinFondo(:))]);
 
 %% Segmentar imagen
+%segmenatacion
+threshold = 0.46;
+% Segmentación por umbralado
+segm = ISinFondo;
+segm(ISinFondo <= threshold) = 1;
+segm(ISinFondo > threshold) = 0;
+figure('name','Imagen preprocesada y su segmentación por umbralado');
+imshow(segm);
 
+%%
+%CURVA ROC
+
+%Image_01R  - Original
+%Image_01R_1stHO - Segmentada 1
+%Image_01R_2ndHO - Segmentada 2
+IVector = ISinFondo(:);
+numPos = 5 ;
+numNeg = 100 ;
+labels = [ones(1, numPos) -ones(1,numNeg)] ;
+scores = randn(size(labels)) + labels ;
+[TPR,TNR] = vl_roc(labels, scores);
+plotroc(TNR,TPR)
+
+
+
+        
